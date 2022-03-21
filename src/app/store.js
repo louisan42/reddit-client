0a7726a244a3e467fc0d3325300170f1281d3506
Louis Amoah-nuamah <louisan42@gmail.com>
2022-03-20 23:15:38 -0400

    placeholder update

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/dist/query';
-import contentSlice from '../features/contents/contentSlice';
 import subredditSlice from '../features/Subreddits/subredditSlice';
 import { redditApi } from './api/apiSlice';
 
 export const store = configureStore({
   reducer: {
-    posts: contentSlice,
     subreddit: subredditSlice,
     [redditApi.reducerPath] : redditApi.reducer,
   },
--- src/features/contents/Contents.js
+++ src/features/contents/Contents.js
@@ -1,36 +1,54 @@
 import React from "react";
 import ContentTile from "../contentTile/ContentTile";
-import { Spinner } from "react-bootstrap";
-//import { getSubredditContent } from "../../app/api/apiSlice";
+import { Card, Placeholder } from "react-bootstrap";
 import { redditApi } from "../../app/api/apiSlice";
 import { useSelector } from "react-redux";
 
-
-
 const Contents = () => {
+  const subreddit = useSelector((state) => state.subreddit.name);
 
-  const subreddit = useSelector(state=> state.subreddit.name)
-  console.log(subreddit)
+  const { data, isFetching } = redditApi.endpoints.getSubreddit.useQuery(
+    `${subreddit}`
+  );
+  const placeholders = [
+    { src: "../../../placeholder-image.png" },
+    { src: "../../../placeholder-image.png" },
+    { src: "../../../placeholder-image.png" },
+    { src: "../../../placeholder-image.png" },
+  ];
 
-  const{data, isLoading} = redditApi.endpoints.getSubreddit.useQuery(`${subreddit}`)
-    const style = { position: "fixed", top: "50%", left: "50%" };
+  return (
+    <>
+      {!isFetching
+        ? data.data.children.map((subreddit, index) => (
+            <ContentTile key={index} subreddit={subreddit.data} />
+          ))
+        : placeholders.map((placeholder, index) => (
+            <Card
+              key={index}
+              border="0"
+              className="p-4 bg-white shadow rounded mb-4"
+            >
+              <Card.Img
+                variant="bottom"
+                src={placeholder.src}
+                className=""
+              />
 
-
-    return (
-      <>
-      {
-            !isLoading ? (
-                
-                data.data.children.map((subreddit,index) =>(
-                 <ContentTile key = {index} subreddit = {subreddit.data}/>
-                ))
-           ) : (<Spinner animation="border" role="status" style={style}>
-            <span className="visually-hidden">Loading</span>
-          </Spinner>)
-        }
-        
-      </>
-    );
-}
+              <Card.Body className="ps-1">
+                <Placeholder as={Card.Title} animation="glow">
+                  <Placeholder xs={6} />
+                </Placeholder>
+                <Placeholder as={Card.Text} animation="glow">
+                  <Placeholder xs={7} /> <Placeholder xs={4} />{" "}
+                  <Placeholder xs={4} /> <Placeholder xs={6} />{" "}
+                  <Placeholder xs={8} />
+                </Placeholder>
+              </Card.Body>
+            </Card>
+          ))}
+    </>
+  );
+      }
 
 export default Contents;
